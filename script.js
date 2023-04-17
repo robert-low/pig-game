274bc768ab8446b2f36a0256721e9ae1c0f8cc3f
Robert Low <robert.low91@gmail.com>
2023-04-17 18:32:52 +0100

    element selects

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// Selecting elements
+const score0Element = document.querySelector('#score--0');
+const score1Element = document.getElementById('score--1'); // Faster to select by ID
+const diceElement = document.querySelector('.dice');
+
+score0Element.textContent = 0;
+score1Element.textContent = 0;
+
+diceElement.classList.add('hidden');
